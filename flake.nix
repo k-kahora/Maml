c29183801f4e6e8cbbe29fe7eab91e31559bcc0d
k-kahora <mango22mjk@gmail.com>
2024-05-14 18:41:25 +0000

    Updated nix flake for builds

--- flake.nix
+++ flake.nix
@@ -4,22 +4,41 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    # Do precise filtering of files in the nix store
-    nix-filter.url = "github:numtide/nix-filter";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nix-filter}:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         legacyPackages = nixpkgs.legacyPackages.${system};
         ocamlPackages = legacyPackages.ocamlPackages;
         lib = legacyPackages.lib;
+
+        # Clean the source directory excluding specific directories or files
+        src = lib.cleanSourceWith {
+          src = ./.;
+          filter = name: type: lib.cleanSourceFilter name type && ! (lib.hasSuffix ".git" name);
+        };
+
+        buildOcamlPackage = { name, buildInputs }: legacyPackages.stdenv.mkDerivation {
+          inherit name src;
+          buildInputs = with ocamlPackages; [
+            dune_3
+            ocaml
+            merlin
+            utop
+          ] ++ buildInputs;
+          buildPhase = ''
+            dune build @install
+          '';
+          installPhase = ''
+            dune install --prefix=$out
+          '';
+        };
+
       in
       {
-      devShells = {
-        default = legacyPackages.mkShell {
+        devShells.default = legacyPackages.mkShell {
           packages = [
-
             legacyPackages.nixpkgs-fmt
             legacyPackages.tree-sitter
             legacyPackages.gcc
@@ -28,31 +47,35 @@
             legacyPackages.ocaml
             legacyPackages.tree-sitter-grammars.tree-sitter-ocaml
             legacyPackages.opam
-            #legacyPackages.nodejs_21 # needed for tree sitter
             legacyPackages.rlwrap
-            legacyPackages.just # like a makefile
+            legacyPackages.just
             ocamlPackages.odoc
             ocamlPackages.ocamlc-loc
             ocamlPackages.ocaml-lsp
             ocamlPackages.alcotest
-            ocamlPackages.zarith # Big ints
+            ocamlPackages.zarith
             ocamlPackages.ocamlformat-rpc-lib
             ocamlPackages.utop
-            # ocamlPackages.angstrom
             ocamlPackages.core
             ocamlPackages.core_unix
-            # ocamlPackages.re
             ocamlPackages.fmt
-            # ocamlPackages.batteries
             ocamlPackages.dune_3
             ocamlPackages.ocaml
             ocamlPackages.merlin
-            ocamlPackages.dot-merlin-reader # merlin needs this to read .merlin file
-
+            ocamlPackages.dot-merlin-reader
           ];
         };
 
-       };
-     }
+        defaultPackage = packages.myOcamlApp;
+        packages = {
+          myOcamlApp = buildOcamlPackage {
+            name = "my-ocaml-app";
+            buildInputs = [
+              ocamlPackages.core
+              ocamlPackages.core_unix
+            ]; # Add additional dependencies here
+          };
+        };
+      }
     );
 }
