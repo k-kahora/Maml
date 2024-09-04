b1f383b0881dfa5512b858e8fa3ed9d80105daf0
k-kahora <mango22mjk@gmail.com>
2024-09-04 21:32:27 +0000

    Compiling free variables with scopes halfway

--- lib/symbol_table/symbol_table.ml
+++ lib/symbol_table/symbol_table.ml
@@ -1,10 +1,12 @@
 let ( let* ) = Result.bind
 
-type symbol_scope = GLOBAL | LOCAL | BUILTIN
+type symbol_scope = GLOBAL | LOCAL | BUILTIN | FREE
 
 let scope_to_string = function
   | GLOBAL ->
       "GLOBAL"
+  | FREE ->
+      "FREE"
   | LOCAL ->
       "LOCAL"
   | BUILTIN ->
@@ -19,7 +21,7 @@ let string_of_symbol symb =
     (scope_to_string symb.scope)
     symb.index
 
-let string_of_store _symb = Format.sprintf "store string not yet implemented"
+let string_of_store _symb = Format.sprintf ""
 
 let symbol_pp fmt symb = Format.fprintf fmt "%s" (string_of_symbol symb)
 
@@ -30,7 +32,10 @@ let n_symbol name scope index = {name; scope; index}
 module StringMap = Map.Make (String)
 
 type symbol_table =
-  {store: symbol StringMap.t; num_definitions: int; outer: symbol_table option}
+  { mutable store: symbol StringMap.t
+  ; num_definitions: int
+  ; outer: symbol_table option
+  ; mutable free_symbols: symbol list }
 
 let define_builtin index name st =
   let symbol = {name; index; scope= BUILTIN} in
@@ -38,7 +43,7 @@ let define_builtin index name st =
   (symbol, {st with store})
 
 let rec symbol_table_string symb_tb =
-  Format.sprintf "{store=%s; num_definitions=%d; outer=%s}"
+  Format.sprintf "{store=%s; num_definitions=%d; outer=%s; }"
     (string_of_store symb_tb.store)
     symb_tb.num_definitions
     (Option.fold ~none:"None" ~some:symbol_table_string symb_tb.outer)
@@ -51,10 +56,23 @@ let symbol_table_eq st1 st2 = StringMap.equal ( = ) st1.store st2.store
 let alc_symbol_table = Alcotest.testable symbol_table_pp symbol_table_eq
 
 let new_symbol_table () =
-  {store= StringMap.empty; num_definitions= 0; outer= None}
+  {store= StringMap.empty; num_definitions= 0; outer= None; free_symbols= []}
 
 let new_enclosed_symbol_table symbol_table =
-  {store= StringMap.empty; num_definitions= 0; outer= Some symbol_table}
+  { store= StringMap.empty
+  ; num_definitions= 0
+  ; outer= Some symbol_table
+  ; free_symbols= [] }
+
+let define_free original symbol_table =
+  let free_symbols = symbol_table.free_symbols @ [original] in
+  let symbol =
+    n_symbol original.name FREE (List.length symbol_table.free_symbols)
+  in
+  let store = StringMap.add original.name symbol symbol_table.store in
+  symbol_table.store <- store ;
+  symbol_table.free_symbols <- free_symbols ;
+  symbol
 
 let define name st =
   let scope = Option.fold ~none:GLOBAL ~some:(fun _ -> LOCAL) st.outer in
@@ -66,17 +84,28 @@ let define name st =
   symbol
 
 let resolve name st =
-  let error = Code.CodeError.SymbolNotFound ("Global symbol not found", name) in
+  let error = Code.CodeError.SymbolNotFound ("GLOBAL", name) in
   let rec resolve_helper outer_st =
-    (* First if the outerscope is Null return an error *)
+    (* First if the outerscope is Null return an error  *)
     let* current_st = Option.to_result ~none:error outer_st in
+    (* First if the outerscope is Null return an error  *)
     let found = StringMap.find_opt name current_st.store in
     (* If there is another outer scope and we did not find the object *)
     if Option.is_none found then resolve_helper current_st.outer
-    else Ok (Option.get found)
+    else
+      let symbol = Option.get found in
+      match symbol.scope with
+      | GLOBAL | BUILTIN ->
+          Ok symbol
+      | _ ->
+          let free = define_free symbol current_st in
+          print_endline "length" ;
+          print_int (List.length current_st.free_symbols) ;
+          print_endline "in free" ;
+          Ok free
   in
   (* First we find if it is in the current scope if not we try to resolve it by moving up the scope *)
   StringMap.find_opt name st.store
-  |> Option.fold ~none:(resolve_helper st.outer) ~some:(fun a -> Ok a)
+  |> Option.fold ~none:(resolve_helper st.outer) ~some:Result.ok
 (* |> Option.to_result *)
 (*      ~none:(Code.CodeError.SymbolNotFound ("Global symbol not found", name)) *)
--- test/symbol_table_test.ml
+++ test/symbol_table_test.ml
@@ -115,6 +115,100 @@ let[@ocaml.warning "-26"] test_resolve_local_nested () =
   in
   List.iter helper expected
 
+(* let a = 1; *)
+(* let b = 2; *)
+
+(* let firstLocal = fn() { *)
+(*   let c = 3; *)
+(*   let d = 4; *)
+(*   a + b + c + d; *)
+
+(*   let secondLocal = fn() { *)
+(*     let e = 5; *)
+(*     let f = 6; *)
+(*     a + b + c + d + e + f; *)
+(*   }; *)
+(* }; *)
+(* above code is tested here *)
+let test_resolve_free () =
+  let global = new_symbol_table () in
+  let global, _ = define "a" global in
+  let global, _ = define "b" global in
+  let first_local = new_enclosed_symbol_table global in
+  let first_local, _ = define "c" first_local in
+  let first_local, _ = define "d" first_local in
+  let second_local = new_enclosed_symbol_table first_local in
+  let second_local, _ = define "e" second_local in
+  let second_local, _ = define "f" second_local in
+  let expected =
+    let n = n_symbol in
+    [ ( first_local
+      , [n "a" GLOBAL 0; n "b" GLOBAL 1; n "c" LOCAL 0; n "d" LOCAL 1]
+      , [] )
+    ; ( second_local
+      , [ n "a" GLOBAL 0
+        ; n "b" GLOBAL 1
+        ; n "c" FREE 0
+        ; n "d" FREE 1
+        ; n "e" LOCAL 0
+        ; n "f" LOCAL 1 ]
+      , [n "c" FREE 0; n "d" FREE 1] ) ]
+  in
+  (* Alcotest.(check (result alc_symbol Code.CodeError.alcotest_error)) *)
+  (*   "Checking resolve a and b" (Ok symbol) *)
+  (*   (resolve symbol.name table) ) *)
+  let helper (table, expected_symbols, expected_free_symbols) =
+    let iter_symbols symbol =
+      let result = resolve symbol.name table in
+      Alcotest.(check (result alc_symbol Code.CodeError.alcotest_error))
+        "checking resolve" (Ok symbol) result
+    in
+    let iter_free_symbols index free_symbol =
+      match table.free_symbols with
+      (* the scope needs to have symbols *)
+      | [] ->
+          ()
+      | _ ->
+          let result = Ok (List.nth table.free_symbols index) in
+          Alcotest.(check (result alc_symbol Code.CodeError.alcotest_error))
+            "checking resolve" (Ok free_symbol) result
+    in
+    List.iter iter_symbols expected_symbols ;
+    List.iteri iter_free_symbols expected_free_symbols
+  in
+  List.iter helper expected
+
+let test_unresolved_free () =
+  let global = new_symbol_table () in
+  let global, _ = define "a" global in
+  let first_local = new_enclosed_symbol_table global in
+  let first_local, _ = define "c" first_local in
+  let second_local = new_enclosed_symbol_table first_local in
+  let second_local, _ = define "e" second_local in
+  let second_local, _ = define "f" second_local in
+  let expected =
+    let n = n_symbol in
+    [n "a" GLOBAL 0; n "c" FREE 0; n "e" LOCAL 0; n "f" LOCAL 1]
+  in
+  (* Alcotest.(check (result alc_symbol Code.CodeError.alcotest_error)) *)
+  (*   "Checking resolve a and b" (Ok symbol) *)
+  (*   (resolve symbol.name table) ) *)
+  let helper expected_symbol =
+    let result = resolve expected_symbol.name second_local in
+    Alcotest.(check (result alc_symbol Code.CodeError.alcotest_error))
+      "checking resolve" (Ok expected_symbol) result
+  in
+  let helper_unresolve name =
+    let result = resolve name second_local in
+    Alcotest.(check (result alc_symbol Code.CodeError.alcotest_error))
+      "checking resolve"
+      (Error (Code.CodeError.SymbolNotFound ("GLOBAL", name)))
+      result
+  in
+  let expected_unresolve = ["b"; "d"] in
+  List.iter helper_unresolve expected_unresolve ;
+  List.iter helper expected
+
 let[@ocaml.warning "-26"] test_define_resolve_builtin () =
   let expected =
     StringMap.(
@@ -145,4 +239,7 @@ let () =
     ; ( "reslove local nested"
       , [Alcotest.test_case "nested" `Quick test_resolve_local_nested] )
     ; ( "resolve and define builtins"
-      , [Alcotest.test_case "builtin" `Quick test_define_resolve_builtin] ) ]
+      , [Alcotest.test_case "builtin" `Quick test_define_resolve_builtin] )
+    ; ( "resolve and define free"
+      , [ Alcotest.test_case "free" `Quick test_resolve_free
+        ; Alcotest.test_case "free" `Quick test_unresolved_free ] ) ]
